1390ac4024a07b521a2305bea27ad4e21f99165c
Daniel <daniel.daramola@localtechnocrat.com>
2024-11-26 16:51:29 +0100

    Implement email verification, format response data for signup, and create verification email endpoint

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,12 +1,20 @@
 import express from "express";
-import { check } from "express-validator";
-import { logIn, logOut, signUp } from "../controllers/auth.controller";
-import { signUpValidationRules } from "../validators/auth.validators";
+import {
+  logIn,
+  logOut,
+  signUp,
+  verifyEmail,
+} from "../controllers/auth.controller";
+import {
+  signUpValidationRules,
+  verifyEmailValidationRules,
+} from "../validators/auth.validators";
 
 const router = express.Router();
 
 router.post("/signup", signUpValidationRules, signUp);
 router.post("/login", logIn);
 router.post("/logout", logOut);
+router.post("/verify-email", verifyEmailValidationRules, verifyEmail);
 
 export default router;
