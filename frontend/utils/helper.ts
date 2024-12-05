b14c1a5933397bcb1ff37f4efb9107029a607fc1
Daniel <daniel.daramola@localtechnocrat.com>
2024-12-05 17:54:51 +0100

    Fix build error

--- frontend/utils/helper.ts
+++ frontend/utils/helper.ts
@@ -1,12 +1,11 @@
-import { User } from "@/contexts/UserContext";
+// import { User } from "@/contexts/UserContext";
 
-export const getUserInitials = (user: { user: User } | null): string => {
-  if (!user || !user.user.firstName || !user.user.lastName) {
-    return "";
-  }
-
-  const firstInitial = user.user.firstName.charAt(0).toUpperCase(); // First letter of first name
-  const lastInitial = user.user.lastName.charAt(0).toUpperCase(); // First letter of last name
-
-  return firstInitial + lastInitial; // Combine them for initials
+export const getUserInitials = (
+  user: { firstName: string; lastName: string } | null
+): string => {
+  if (!user) return ""; // Handle null or undefined user gracefully
+  const { firstName, lastName } = user;
+  const firstInitial = firstName?.charAt(0) || ""; // Get the first character of the first name
+  const lastInitial = lastName?.charAt(0) || ""; // Get the first character of the last name
+  return `${firstInitial}${lastInitial}`.toUpperCase(); // Combine and capitalize
 };
