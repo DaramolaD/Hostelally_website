0f7844d4d150e26a9f0f003fb488bb393e67ec47
Daniel <daniel.daramola@localtechnocrat.com>
2024-12-19 21:39:44 +0100

    add the booking, rooms and setting page

--- backend/db/connectDB.ts
+++ backend/db/connectDB.ts
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    mongoose.set('strictQuery', true)
+  mongoose.set("strictQuery", true);
   try {
     const connect = await mongoose.connect(process.env.MONGODB_URL as string);
     console.log(`Database connected sucessfully ${connect.connection.host}`);
   } catch (error: any) {
     console.log("Error connecting to MongoDB: ", error.message);
-    process.exit(1) // 1 is failure, 0 status code is success
+    process.exit(1); // 1 is failure, 0 status code is success
   }
 };
+
+// what is the use for mongomemoryserver in express
